c64fd6f095357dae3a9f5eec893b561cfbb34de3
owncreed93 <christiantarifer@gmail.com>
2020-08-10 17:24:55 -0500

    Testing Prototype pattern

---
+++ Creational/exercise-02/index.js
@@ -0,0 +1,22 @@
+const Users = (() => {
+    const recurso = 'https://jsonplaceholder.typicode.com/users'
+
+    return {
+
+        listar: async () => {
+
+            return await fetch (recurso).then( x => x.json() )
+
+        },
+
+        crear: async (data) => {
+
+            return await fetch (recurso, { type: 'POST', body: JSON.stringify(data) }).then( x => x.json() )
+
+        },
+
+
+
+    }
+
+}) ()
